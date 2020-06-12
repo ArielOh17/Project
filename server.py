bf89a0532e0ab0f00bf4c9281d91d5748f85eb76
ArielOh17 <45701789+ArielOh17@users.noreply.github.com>
2020-06-12 18:34:28 +0300

    server.py

--- server.py
+++ server.py
@@ -8,33 +8,45 @@ server_socket.listen(255)
 open_client_sockets = []
 adress = {}
 open1={}
-connection = sqlite3.connect('sqlite_A.db')
+connection = sqlite3.connect('sqlite_A.db')         #connecting the data base.
 connection.row_factory = sqlite3.Row
 cursor= connection.cursor()
 
-#cursor.execute('CREATE TABLE ArieL (name TEXT PRIMARY KEY, password INTEGER)')
+#cursor.execute('CREATE TABLE Av (name TEXT PRIMARY KEY, password TEXT)')
 
+
+#Name: Ariel Ohev
+#input: name, password
+#output: check if the password is correct and return True or False.
 def log_in(name, password):
     try:
-        cursor.execute('SELECT * FROM ArieL WHERE name=?', (password,))
+        cursor.execute('SELECT * FROM Av WHERE name=?', (name,))
         row = cursor.fetchone()
-        password = row['password']
+        passw = row['password']
         name = row['name']
-
+        if passw!=password:
+            return False
         print(name)
-        print(password)
         return True
 
     except:
         return False
+
+
+#Name: Ariel Ohev
+#input: name, password
+#output: insert the username and password to the datatbase.
 def register(name, password):
+    print(name)
     try:
-        cursor.execute('INSERT INTO ArieL VALUES(?,?)', (name,int(password)))
+        cursor.execute('INSERT INTO Av VALUES(?,?)', (name,str(password)))
         connection.commit()
         return True
     except:
         return False
 
+
+#on this loop the server communicate with the client and chcek if he want's to connect or register account or 2 client's want to make a peer to peer connection.
 while True:
     data = ""
     rlist, wlist, xlist = select.select([server_socket] + open_client_sockets, [], [])
